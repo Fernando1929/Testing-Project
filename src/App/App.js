60aca88df62d99fb2d7b9128d249fbfd87adb829
Luis Quiles <luis.quiles1@upr.edu>
2021-08-10 13:33:23 -0500

    Added the about us page

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import Home from "../Pages/Home/home";
-import Map from "../Pages/Map/map"
+import Map from "../Pages/Map/map";
+import About from "../Pages/About/aboutus";
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path = "/" render={() => <Home/>}/>
         <Route exact path = "/map" render={() => <Map/>}/>
+        <Route exact path = "/aboutus" render ={() => <About/>}/>
       </Switch>
     </Router>
       
---
+++ src/Pages/About/aboutus.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./aboutus.css";
+import About from "../../Components/About";
+
+function about(){
+return (
+    <About/>
+    );
+}
+
+export default about;
