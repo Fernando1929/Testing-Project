e2e697884e026936a6277c54233b9802e7d6f709
Luis Quiles <luis.quiles1@upr.edu>
2021-08-12 18:49:40 -0500

    Merged

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,6 @@ function About(props) {
           rel="stylesheet"
           href="https://www.w3schools.com/w3css/4/w3.css"
         />
-        {/* Navbar (sit on top) */}
-        <Navbar />
-        {/* Header */}
         <header
           className="w3-display-container w3-content w3-wide"
           style={{ maxWidth: 1500 }}
@@ -323,51 +320,6 @@ function About(props) {
         }}
       />
       {/* Page content */}
-<<<<<<< HEAD
-      <div
-        className="w3-black w3-content"
-        style={{ maxWidth: 2000, marginTop: 8 }}
-      >
-        {/* The Band Section */}
-        <div
-          className="w3-container w3-content w3-center w3-padding-64"
-          style={{ maxWidth: 800 }}
-          id="band"
-        >
-          <h2 className="w3-wide">Amazon Prime Z</h2>
-          <p className="w3-opacity">
-            <i>Amazon today, amazon tomorrow, amazon forever</i>
-          </p>
-
-          <img
-            src="amazonhq.jpg"
-            alt="Me"
-            className="w3-image"
-            style={{ display: "block", margin: "auto" }}
-            width={900}
-            height={533}
-          />
-
-          <p className="w3-justify w3-padding-64">
-            In the year 2069 an infected BigMac help spread a new virus
-            throughout the world. The newly discovered Z virus turned people
-            into zombies that ravaged the world. No one knows where the virus
-            came from or why it turns people into zombies. Amazon, at the time
-            as the world’s richest most prominent company, took control of the
-            United States and established order. Amazon divided the remaining
-            survivors into 3 factions and left the independent territory of
-            Florida to its own resources. Amazon uses its new service, titled
-            “Amazon Prime Z” to help the district and its residence keep up with
-            inventory. It also allows new residents to join or change factions.
-            Amazon watches from its headquarters afar as the residents of the
-            factions struggle to survive, “Unable” to interfere, but always
-            there to help
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-=======
       <div className="w3-black w3-content" style={{ maxWidth: 2000, marginTop: 0 }}>
         
           <div className="w3-container" id="designers" style={{ marginTop: 0 }}>
@@ -484,6 +436,5 @@ function About(props) {
     );
 
 
->>>>>>> 1e6605d0c0535cd8484662f07f321e53020f1dff
 }
 export default About;
