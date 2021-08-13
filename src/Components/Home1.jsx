eeb75287816a71bed080f99c896e7ee23e86a422
angelus <angel.thecoolest.009@gmail.com>
2021-08-12 22:01:22 -0400

    Made footer smaller, Finished home page

--- src/Components/Home1.jsx
+++ src/Components/Home1.jsx
@@ -27,30 +27,46 @@ function Home1(props) {
   />
   {/* Page content */}
   <div className="w3-black w3-content" style={{ maxWidth: 2000, marginTop: 8 }}>
+
+
+  <div className="w3-container w3-content w3-center w3-black"
+      style={{ maxWidth: 1500  }} >
+    
+            {/* Header */}
+              <header
+                className="w3-display-container w3-content w3-wide "
+                style={{ maxWidth: 1500 }}
+                id="home">
+
+                <h1 className="w3-wide">Amazon Prime Z</h1>
+                  <p className="w3-opacity">
+                    <i>Amazon today, amazon tomorrow, amazon forever</i>
+                </p>
+
+                <img
+                  className="w3-image"
+                  src="amazonhq.jpg"
+                  alt="Architecture"
+                  width={1800}
+                  height={800}/>
+              </header>
+  </div>
+  {/*END Container 1*/} 
+
     {/* The Band Section */}
     <div
       className="w3-container w3-content w3-center w3-padding-32"
       style={{ maxWidth: 800 }}
       id="band"
     >
-      <h1 className="w3-wide">Amazon Prime Z</h1>
-      <p className="w3-opacity">
-        <i>Amazon today, amazon tomorrow, amazon forever</i>
-      </p>
-
-      <img
-        src="amazonhq.jpg"
-        alt="Me"
-        className="w3-image"
-        style={{ display: "block", margin: "auto" }}
-        width={900}
-        height={533}
-      />
+      <h1 className="w3-wide">Our History</h1>
+     
+ 
 
 <p className="w3-opacity">
         <i>Amazon HQ</i>
       </p>
-      <p className="w3-justify w3-padding-64">
+      <p className="w3-justify w3-padding-16">
       In the year 2069 an infected BigMac help spread a new virus throughout the world. 
       The newly discovered Z virus turned people into zombies that ravaged the world. 
       No one knows where the virus came from or why it turns people into zombies. 
@@ -61,7 +77,25 @@ function Home1(props) {
       It also allows new residents to join or change factions. Amazon watches from its headquarters afar 
       as the residents of the factions struggle to survive, “Unable” to interfere, but always there to help.
       </p>
-      
+
+      <p className="w3-justify w3-padding-16">
+      Amazon HQ, located in the former state of Nebraska, is in the heart of the NoNo zone,
+       and thus is unfortunately inaccessible to the surrounding factions. Even so, Amazon is 
+       an impenetrable fortress that established the faction system in place now. It was created 
+       by the wealthiest members of society. The factions are free to do as they please, but Amazon 
+       helps them keep track of inventory and survivor population. Amazon wishes for the survival of
+        humanity within the factions, as such, any allegations that amazon amasses zombies to send hordes 
+        towards the faction boarders for entertainment and research is false.
+
+      </p>
+        <img
+        src="hqirl.jpg"
+        alt="Me"
+        className="w3-image"
+        style={{ display: "block", margin: "auto" }}
+        width={900}
+        height={533}
+      />
     </div>
   </div>
 </div>
